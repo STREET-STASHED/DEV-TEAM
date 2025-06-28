4e4dc23a041ed1f03316aa35312ce8cda07e4494
streetstashed <streetstashed412@gmail.com>
2025-06-28 11:18:16 -0400

    Refactor: Update AuthScreen to use redirectUserBasedOnProfile for user redirection; streamline import and variable declaration

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import useOnboardingRedirect from '../hooks/useOnboardingRedirect';
+import redirectUserBasedOnProfile from '../hooks/useOnboardingRedirect';
 import { useRouter } from 'next/router';
 import supabase from '../lib/supabaseClient';
 
@@ -11,7 +11,6 @@ const AuthScreen = () => {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [initError, setInitError] = useState<string | null>(null);
   const router = useRouter();
-  const redirectUserBasedOnProfile = useOnboardingRedirect;
 
   const handleRedirect = async (role: string) => {
     switch (role) {
